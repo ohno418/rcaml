ba48c32b29bc0b94dc638f8a1e2f2f6a241cd1e5
ohno418 <yutaro.ono.418@gmail.com>
2022-05-16 17:43:40 +0900

    Allow whitespaces

--- src/eval.rs
+++ src/eval.rs
@@ -1,13 +1,13 @@
 // <program> ::= <expr> ";;"
 pub fn eval(input: String) -> Result<String, String> {
     let expr = match input.find(";;") {
-        Some(idx) => &input[0..idx],
+        Some(idx) => (&input[0..idx]).trim(),
         None => return Err(r#"";;" is required at the end of a expression"#.to_string()),
     };
 
     match expr.parse::<i64>() {
         Ok(n) => Ok(n.to_string()),
-        Err(_) => Err(format!(r#"Cannot parse "{}" into int"#, input)),
+        Err(_) => Err(format!(r#"Cannot parse "{}" into int"#, expr)),
     }
 }
 
@@ -35,5 +35,13 @@ mod tests {
     fn cannot_parse_not_integer_input() {
         let input = "123abc;;".to_string();
         assert!(eval(input).is_err())
+    }
+
+    #[test]
+    fn parses_input_with_spaces() {
+        let input = " 123  ;;".to_string();
+        let expected = "123";
+        let actual = eval(input).unwrap();
+        assert_eq!(expected, actual);
     }
 }
