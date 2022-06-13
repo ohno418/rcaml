07325c16f32ba620b0e0b12b3862a5543a3abd91
ohno418 <yutaro.ono.418@gmail.com>
2022-06-13 22:04:10 +0900

    (wip) Trying to split Ty into a type and its value

--- src/ty/mod.rs
+++ src/ty/mod.rs
@@ -5,8 +5,8 @@ pub(crate) use list::List;
 // value with its type
 #[derive(Clone, Debug, PartialEq)]
 pub(crate) enum Ty {
-    Int(i64),   // int
-    Bool(bool), // bool
-    List(List), // list
-    Fn,         // function
+    Int(i64),      // int
+    Bool(bool),    // bool
+    List(Box<Ty>), // list
+    Fn,            // function
 }
